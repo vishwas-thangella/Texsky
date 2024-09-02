1a72a8adebef28d4c273bb356e5460431ba771aa
Vishwas <vishwas56883669@gmail.com>
2024-09-02 21:55:06 +0530

    space adjustments

--- texsky/settings.py
+++ texsky/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(#i_0sm-h!m3lpy%%q@f^dlw&w4h$!8=9r(*0-pc4wcnbv+k&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
